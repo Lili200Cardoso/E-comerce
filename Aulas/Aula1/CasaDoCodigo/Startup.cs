e15ef1ae4ae4a39f31edbc00fcf5a2b7620208c4
Lili Cardoso <lili200cardoso@outlook.com.br>
2023-01-31 19:56:00 -0300

    Conectar com banco de dados | Migrations

--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,10 +23,17 @@ namespace CasaDoCodigo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("CasaDoCodigo");
+            services.AddDbContext<ApplicationContext>(options =>
+
+                options.UseSqlServer(connectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +53,11 @@ namespace CasaDoCodigo
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+            //EnsureCreated()
+            //caso o banco de dados não exista, esse comando o criará!
+            //O único problema  é que depois que vc usa esse método vc não poderá mais gerar nenhuma migration no seu sistema!
+            //Por isso trocaremos esse método pelo o método Migrate();
+            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
         }
     }
 }
